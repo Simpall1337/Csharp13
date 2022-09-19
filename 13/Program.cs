bd8da767827389c3ad1c82f3ed19cd24515751ce
Alex Huzo <sanduguzo1337@gmail.com>
2022-09-19 13:26:52 +0300

    13

---
+++ 13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Lessons
+{
+    internal class Program
+    {
+        static void SumNumber(ref int Number, ref int KeepNumber, int i = 0)
+        {
+            if (i == 100)
+                return;
+            KeepNumber = KeepNumber + Number % 10;
+            Number = Number / 10;
+            i++;
+            SumNumber(ref Number, ref KeepNumber, i);
+        }
+        static void Main(string[] args)
+        {
+            int KeepNumber = 0;
+            int Number = 1234567890;
+            SumNumber(ref Number, ref KeepNumber);
+            Console.WriteLine(KeepNumber);
+        }
+    }
+}
